33c03b7801a524c2086fb496344586975e77ed58
Bence Varga <Bence.Varga2@ibm.com>
2025-05-23 15:00:41 +0200

    Add notification system for cart actions

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {uiActions} from "./ui-slice";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -40,6 +41,52 @@ const cartSlice = createSlice({
         },
     },
 });
+
+
+
+export const sendCartData = ( cart ) => {
+    return async (dispatch) => {
+        dispatch(
+            uiActions.showNotification({
+                status: 'pending',
+                title: 'Sending...',
+                message: 'Sending cart data!',
+            })
+        );
+
+        const sendRequest = async () => {
+            const response = await fetch(
+                'https://react-http-c79c9-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.');
+            }
+
+        }
+
+        try {
+            await sendRequest();
+        } catch (error) {
+            dispatch(
+                uiActions.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!',
+                })
+            );
+        }
+        dispatch(uiActions.showNotification({
+            status: 'success',
+            title: 'Success!',
+            message: 'Sent cart data successfully!'
+        }))
+    }
+};
 
 export const cartActions = cartSlice.actions;
 
